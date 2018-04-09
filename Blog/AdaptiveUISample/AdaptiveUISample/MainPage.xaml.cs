04aadcf8b02bf06667c7a51fc465ad1cd96aa6e4
LittleFish <3478628675@qq.com>
2018-04-10 02:56:18 +0800

    Add files via upload

---
+++ Blog/AdaptiveUISample/AdaptiveUISample/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace AdaptiveUISample
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            /*
+            * C#事件订阅
+            * Lambda表达式，s和e为传入参数
+            */
+            this.SizeChanged += (s, e) =>
+            {
+                var state = "WideLayout";
+                if (e.NewSize.Width > 000 && e.NewSize.Width < 600)
+                {
+                    state = "NarrowLayout";
+                }
+                VisualStateManager.GoToState(this, state, true);
+            };
+        }
+    }
+}
