239b93919041fce6d74c5b09d4c163ad37542cb6
LittleFish <3478628675@qq.com>
2018-04-12 01:47:40 +0800

    共享UI

---
+++ Blog/ShowShareUISample/README.md
@@ -0,0 +1,6 @@
+#### 现代操作系统应用开发：UWP——UI共享：DataTransferManager 源代码
+
+#### 博客地址：http://littlefish33.cn/uwp/ShowShareUI
+
+- 运行环境：Windows 10
+- 开发工具：Visual Studio 2017
---
+++ Blog/ShowShareUISample/ShowShareUISample/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace ShowShareUISample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //使ShareRequested函数在共享操作时发生
+            DataTransferManager.GetForCurrentView().DataRequested += ShareRequested;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //启动共享
+            DataTransferManager.ShowShareUI();
+        }
+
+        //设置想要共享的内容
+        private void ShareRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var deferral = args.Request.GetDeferral();
+            DataRequest request = args.Request;
+            request.Data.Properties.Title = "ShareUISample";
+            request.Data.SetText("Description：" + "This is a line from ShareUISample. Welcome to learn UWP.");
+            request.Data.SetBitmap(RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/flash.jpg")));
+            deferral.Complete();
+        }
+    }
+}
