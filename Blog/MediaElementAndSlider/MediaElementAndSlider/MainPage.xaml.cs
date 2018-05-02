4e27030574299a7e6fc6803de8751327b0f22175
LittleFish <3478628675@qq.com>
2018-05-02 20:26:07 +0800

    MediaElement和Slider绑定

---
+++ Blog/MediaElementAndSlider/MediaElementAndSlider/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace MediaElementAndSlider
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // 开始播放
+        private void playClick(object sender, RoutedEventArgs e)
+        {
+            mediaSimple.Play();
+            EllStoryboard.Begin();
+            progress.Maximum = mediaSimple.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+
+        // 暂停播放
+        private void pauseClick(object sender, RoutedEventArgs e)
+        {
+            mediaSimple.Pause();
+            EllStoryboard.Pause();
+        }
+
+        // 停止播放，回到起始位置
+        private void stopClick(object sender, RoutedEventArgs e)
+        {
+            mediaSimple.Stop();
+            EllStoryboard.Stop();
+        }
+
+    }
+}
---
+++ Blog/MediaElementAndSlider/README.md
@@ -0,0 +1,6 @@
+#### 现代操作系统应用开发：MediaElement播放进度和Slider滑块的绑定 源代码
+
+#### 博客地址：http://littlefish33.cn/uwp/MediaElementAndSlider
+
+- 运行环境：Windows 10
+- 开发工具：Visual Studio 2017
